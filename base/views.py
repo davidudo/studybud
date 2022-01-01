67a6ea56d4766be13098f59f9c637921a1f7d3cf
David Udo <udodavid46.ud@gmail.com>
2022-01-01 23:16:53 +0100

    Worked on the User model

--- base/views.py
+++ base/views.py
@@ -3,12 +3,10 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 
-from .models import Room, Topic, Message
-from .forms import RoomForm, UserForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here
 
@@ -26,17 +24,17 @@ def loginPage(request):
     return redirect('home')
     
   if request.method == 'POST':
-    username = request.POST.get('username').lower()
+    email = request.POST.get('email').lower()
     password = request.POST.get('password')
     
     try:
-      user = User.objects.get(username = username)
+      user = User.objects.get(email = email)
     except:
       messages.error(request, 'User does not exist')
       
     user = authenticate(
       request, 
-      username = username,
+      email = email,
       password = password
     )
     
@@ -44,7 +42,7 @@ def loginPage(request):
       login(request, user)
       return redirect('home')
     else:
-      messages.error(request, 'Username or password does not exist')
+      messages.error(request, 'Email or password does not exist')
   
   context  = { 'page': page }
   
@@ -59,10 +57,10 @@ def logoutUser(request):
   
   
 def registerPage(request):
-  form = UserCreationForm()
+  form = MyUserCreationForm()
   
   if request.method == 'POST':
-    form = UserCreationForm(request.POST)
+    form = MyUserCreationForm(request.POST)
     if form.is_valid():
       user = form.save(commit = False)
       user.username = user.username.lower()
@@ -204,7 +202,7 @@ def updateRoom(request, pk):
     # if form.is_valid():
     #   form.save()
     
-    return redirect('home')
+    return redirect('room', pk = room.id)
     
 
   
@@ -241,7 +239,7 @@ def deleteMessage(request, pk):
   
   if request.method == 'POST':
     message.delete()
-    return redirect('home')
+    return redirect('room', pk = message.room.id)
   
   return render(request, 'base/delete.html', { 'obj': message })
 
@@ -253,7 +251,7 @@ def updateUser(request):
   form = UserForm(instance = user)
   
   if request.method == 'POST':
-    form = UserForm(request.POST, instance = user)
+    form = UserForm(request.POST, request.FILES, instance = user)
     if form.is_valid():
       form.save()
       return redirect('user-profile', pk = user.id)
