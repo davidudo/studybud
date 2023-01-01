2769fd29a647227cca5089839e7d6c9b8e1266b0
David Udo <udodavid46.ud@gmail.com>
2023-01-01 13:56:12 +0100

    Prepared application for deployment to render

--- studybud/settings.py
+++ studybud/settings.py
@@ -98,15 +98,15 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 # import dj_database_url 
-prod_db  =  dj_database_url.config(default='postgres://studybud_user:hB4C47DFcnaxSdrM4ggkG1clffBRiNgE@dpg-ceongapgp3jlcsk7k9r0-a/studybud', conn_max_age=500)
+prod_db  =  dj_database_url.config('postgres://studybud_user:hB4C47DFcnaxSdrM4ggkG1clffBRiNgE@dpg-ceongapgp3jlcsk7k9r0-a/studybud', conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 
